8d627ed167c69f84b9f1dbdd5d686d1f956f0202
nengdong.lv <nengdong.lv@qunar.com>
2024-01-30 16:41:49 +0800

    测试类

---
+++ mini-tomcat/src/main/java/server/HttpProcessor.java
@@ -0,0 +1,9 @@
+package server;
+
+/**
+ * @Author lnd
+ * @Description
+ * @Date 2024/1/26 17:34
+ */
+public class HttpProcessor {
+}
--- mini-tomcat/src/main/java/server/Servlet.java
+++ mini-tomcat/src/main/java/server/Servlet.java
@@ -1,10 +1,10 @@
-package server;
-
-/**
- * @Author lnd
- * @Description
- * @Date 2024/1/17 15:43
- */
-public interface Servlet {
-    public void service(Request req, Response resp);
-}
+//package server;
+//
+///**
+// * @Author lnd
+// * @Description 废弃
+// * @Date 2024/1/17 15:43
+// */
+//public interface Servlet {
+//    public void service(Request req, Response resp);
+//}
